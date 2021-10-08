5de4a916a584b3bc5bbdc94c737e38ed1dae0375
kubotaseiko <kubota712.050@gmail.com>
2021-10-08 12:06:37 +0900

    practice1008

---
+++ src/task4/Book.java
@@ -0,0 +1,76 @@
+package task4;
+
+import java.util.*;
+
+public class Book implements Comparable<Book>, Cloneable{
+	private String title;
+	private Date publishDate;
+	private String comment;
+	public int hashCoda() {
+		int r = 1;
+		r = 31 * publishDate.hashCode();
+		r = 31 * title.hashCode();
+		return r;
+	}
+
+	public boolean equals(Object o) {
+		if(this == o) {
+			return true;
+		}
+		if(o == null) {
+			return false;
+		}
+		if(!(o instanceof Book)) {
+			return false;
+		}
+		Book b = (Book) o;
+		if(!publishDate.equals(b.publishDate)) {
+			return false;
+		}
+		if(!title.equals(b.title)) {
+			return false;
+		}
+		return true;
+	}
+
+	public int compareTo(Book o) {
+		return this.publishDate.compareTo(o.publishDate);
+	}
+
+	public Book clone(){
+		Book b = new Book();
+		b.title = this.title;
+		b.comment = this.comment;
+		b.publishDate = (Date)this.publishDate.clone();
+		return b;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getPublishDate() {
+		return publishDate;
+	}
+
+	public void setPublishDate(Date publishDate) {
+		this.publishDate = publishDate;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public String toString() {
+		return this.title;
+	}
+
+}
--- src/task4/Main.java
+++ src/task4/Main.java
@@ -1,4 +1,31 @@
 package task4;
+import java.util.*;
 
 public class Main {
+	public static void main(String[] args) {
+
+		Book b1 = new Book();
+		b1.setTitle("テスト");
+		b1.setPublishDate(new Date(1316622225935L));
+		b1.setComment("面白い");
+		System.out.println();
+
+		Book b2 = new Book();
+		b2.setTitle("テスト2");
+		b2.setPublishDate(new Date(1313132755277L));
+		b2.setComment("楽しい");
+		System.out.println();
+
+		ArrayList<Book> list = new ArrayList<Book>();
+		list.add(b1);
+		list.add(b2);
+
+		Collections.sort(list);
+
+//		Collections.sort(Book);
+		System.out.println(list);
+		System.out.println(b1.getTitle());
+		System.out.println(b1.getPublishDate());
+	}
+
 }

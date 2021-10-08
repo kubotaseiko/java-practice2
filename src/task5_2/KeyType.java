adc3cd08170469c734db8adece8db71ee525c19a
kubotaseiko <kubota712.050@gmail.com>
2021-10-08 16:53:24 +0900

    practice1008

---
+++ src/task5_2/KeyType.java
@@ -0,0 +1,3 @@
+package task5_2;
+
+enum KeyType { PADLOCK, BUTTON, DIAL, FINGER; }
---
+++ src/task5_2/Main.java
@@ -0,0 +1,9 @@
+package task5_2;
+
+public class Main {
+	public static void main(String[] args){
+		StrongBox<String> s = new StrongBox<String>(KeyType.PADLOCK);
+		s.get();
+		s.get();
+	}
+}
